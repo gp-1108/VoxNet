b34c4c23b3816c98eec08fbf511e92e5152b510d
gp-1108 <pietrogirotto00@gmail.com>
2024-11-24 12:03:49 +0100

    refactored the sript to make it modular

--- dataset_creation/script.sh
+++ dataset_creation/script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 project_dir=/home/gp1108/Code/VoxNet
-dataset_archive=/ext/ModelNet40.zip
-dataset_path=/ext/ModelNet40
-dataset_output=/ext/ModelNet40Voxel
-sif_image_path=/ext/sif_image.sif
-save_path=/home/gp1108/Code/VoxNet/ModelNet40Voxel.zip
+ext_dir=/ext
+dataset_archive="${ext_dir}/ModelNet40.zip"
+dataset_path="${ext_dir}/ModelNet40"
+dataset_output="${ext_dir}/ModelNet40Voxel"
+sif_image_path="${ext_dir}/sif_image.sif"
+save_path="${ext_dir}/ModelNet40Voxel_128.zip"
 
 url=http://modelnet.cs.princeton.edu/ModelNet40.zip
 gdrive_sif_id=1H6Z9euw7WWRPUz8XGMHPtrgH8cNlt31H
@@ -24,7 +25,11 @@ unzip -q $dataset_archive -d /ext
 
 # Run the script
 cd $project_dir
-singularity exec --bind /ext:/ext $sif_image_path python3 dataset_converter.py --input_dir $dataset_path --output_dir $dataset_output --num_workers 3
+singularity exec --bind /ext:/ext $sif_image_path \
+    python3 dataset_converter.py \
+    --input_dir $dataset_path \
+    --output_dir $dataset_output \
+    --num_workers 3
 
 # Zip the dataset and save it
 zip -r $save_path $dataset_output
