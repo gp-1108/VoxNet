6405c2d39c55e6b5d2497d745ede7bb01d95c7e7
gp-1108 <pietrogirotto00@gmail.com>
2025-01-08 21:15:19 +0100

    model without BN

--- training/models/__init__.py
+++ training/models/__init__.py
@@ -3,6 +3,7 @@ from .BatchNormVoxNet import BatchNormVoxNet
 from .ResVoxNet import ResVoxNet
 from .ResBNVoxNet import ResBNVoxNet
 from .ResBNVox64Net import ResBNVox64Net
+from .ResVox64Net import ResVox64Net
 
 __all__ = [
     "BaseVoxNet",
@@ -10,4 +11,5 @@ __all__ = [
     "ResVoxNet",
     "ResBNVoxNet",
     "ResBNVox64Net",
+    "ResVox64Net",
 ]
